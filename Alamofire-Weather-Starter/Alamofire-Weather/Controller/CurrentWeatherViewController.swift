75603361b9fd503646d7263db7416fcbfda57f45
urvishshah17 <urvishshahuae@gmail.com>
2018-01-06 13:43:13 -0500

    Organize code in MVC and creating and defining View IBOutlet. in Main story board

---
+++ Alamofire-Weather-Starter/Alamofire-Weather/Controller/CurrentWeatherViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CurrentWeatherViewController.swift
+//  Alamofire-Weather
+//
+//  Created by Urvish shah on 2018-01-06.
+//  Copyright © 2018 Duc Tran. All rights reserved.
+//
+
+import UIKit
+
+class CurrentWeatherViewController: UIViewController {
+
+    @IBOutlet weak var backGroundImageView: UIImageView!
+    @IBOutlet weak var tempScaleLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+   
+    let forcastAPIKey = "fed774dd2fdbda09c3e5600631cfa4ce"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+   
+
+
+}
