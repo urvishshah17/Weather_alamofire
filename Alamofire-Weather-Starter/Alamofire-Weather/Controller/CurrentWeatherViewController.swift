ed78bf0f65ed08b26df0fb2601e2deb605775e3e
urvishshah17 <urvishshahuae@gmail.com>
2018-01-06 14:09:38 -0500

    error in Main view controller

--- Alamofire-Weather-Starter/Alamofire-Weather/Controller/CurrentWeatherViewController.swift
+++ Alamofire-Weather-Starter/Alamofire-Weather/Controller/CurrentWeatherViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-
 class CurrentWeatherViewController: UIViewController {
 
     @IBOutlet weak var backGroundImageView: UIImageView!
@@ -16,11 +15,23 @@ class CurrentWeatherViewController: UIViewController {
     @IBOutlet weak var cityLabel: UILabel!
    
     let forcastAPIKey = "fed774dd2fdbda09c3e5600631cfa4ce"
-    
+    let cordinate : (lat: Double, long:Double) = (37.8267,-122.4233)
+    var forcastService :  ForcastService
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        forcastService = ForcastService(APIKey: forcastAPIKey)
+        forcastService.getCurrentWeather(latitude: cordinate.lat, longitude: cordinate.long) { (currentWeather) in
+                if let currentWeather = currentWeather{
+                    DispatchQueue.main.async {
+                        if let temperature = currentWeather.temprature{
+                            self.tempLabel.text="\(temperature)"
+                        }else{
+                            self.tempLabel.text="-"
+                        }
+                }
+            }
+        }
     }
 
    
